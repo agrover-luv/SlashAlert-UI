6705216fdcac1de39609ca604fb79ccf6a54597b
Abhishek Srivastava <abhisheksrivastava@Mac.lan>
2025-10-10 12:34:11 +0530

    Modified home page

--- slash-alert/src/App.js
+++ slash-alert/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Slash Alert Application
         </p>
         <a
           className="App-link"
